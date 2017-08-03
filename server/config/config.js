9ff5100bcb3af799a756bba22ff7e5ebac74d798
timfung93 <timothy.fung.04@gmail.com>
2017-08-03 16:32:48 -0400

    added config.js files for testing database

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+const env = process.env.NODE_ENV || 'development';
+
+
+if(env === 'development') {
+	process.env.PORT = 8080;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp';
+} else if (env === 'test') {
+	process.env.PORT = 8080;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest';
+}
+
+
