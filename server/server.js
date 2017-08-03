e3b3351c1b6818bf7eacb0cf76cb8a36167ff818
timfung93 <timothy.fung.04@gmail.com>
2017-08-03 14:13:24 -0400

    added delete route

--- server/server.js
+++ server/server.js
@@ -62,8 +62,25 @@ app.get('/todos/:id', (req, res) => {
 	});
 
 
-	
+});
 
+
+app.delete('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	Todo.findByIdAndRemove(id).then(todo => {
+		if(!todo) return res.status(404).send();
+		console.log('Deleted todo', todo)
+		res.status(200).send({
+			todo
+		})
+	}).catch(e => {
+		console.log('ID not found')
+		return res.status(404).send(e)
+	});
 });
 
 
